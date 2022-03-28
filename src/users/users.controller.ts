cb0dd7fdc935e9d9df43a9074bbfa9806aaeeeb0
Yang025 <kkadso@gmail.com>
2022-03-29 07:11:25 +0800

    feat: controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ id: 0 }];
+  }
+
+  @Get(':id')
+  getUsersById(@Param('id') id: string): any {
+    return { id: Number(id) };
+  }
+}
