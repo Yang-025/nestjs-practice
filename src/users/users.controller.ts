c2dce93d45484608bfc6147a6d3d8939e65a8c47
Yang025 <kkadso@gmail.com>
2022-04-05 21:26:51 +0800

    feat: entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  getUsers() {
+  getUsers(): User[] {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  getUsersById(@Param('id') id: string): any {
+  getUsersById(@Param('id') id: string): User {
     return this.usersService.findById(Number(id));
   }
 
   @Post()
-  createUser(@Body() body: CreateUserDto): any {
+  createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
   }
 }
